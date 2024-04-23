1abb524cd785acd6d602d3286e9ebf8d3b1378ba
abeer fatima <abeerfatimahom@gmail.com>
2024-04-24 04:11:34 +0500

    calculator

---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "enter first number", type: "number", name: "firstnumber" },
+  { message: "enter second number", type: "number", name: "secondnumber" },
+  { message: "enter third number", type: "number", name: "thirdnumber" },
+  {
+    message: "select one of the operator to continue operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+//conditional sentence
+if(answer.operator==="Addition"){
+    console.log(answer.firstnumber+answer.secondnumber+answer.thirdnumber); 
+}
+else if (answer.operator==="Subtraction"){
+    console.log(answer.firstnumber-answer.secondnumber-answer.thirdnumber);
+}
+else if (answer.operator==="Multiplication"){
+    console.log(answer.firstnumber*answer.secondnumber*answer.thirdnumber);
+}
+else if (answer.operator==="Division"){
+    console.log(answer.firstnumber/answer.secondnumber/answer.thirdnumber);
+} else{ 
+    console.log("please select valid operator")
+}
